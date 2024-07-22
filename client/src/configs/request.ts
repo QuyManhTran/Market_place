662ce6df590429c890b086df0f29e9a8cc0febef
QuyManh <quymanhtran453@gmail.com>
2024-07-22 18:03:55 +0700

    chore: Update Docker configuration and project structure

--- client/src/configs/request.ts
+++ client/src/configs/request.ts
@@ -2,9 +2,7 @@ import { userStore } from '@/zustand/user';
 import axios from 'axios';
 
 const request = axios.create({
-    baseURL:
-        `${process.env.REACT_APP_HOST}/api/${process.env.REACT_APP_API_VERSION}` ||
-        'http://localhost:3333/api/v1',
+    baseURL: `${process.env.REACT_APP_HOST}/api/${process.env.REACT_APP_API_VERSION}`,
     withCredentials: true,
     timeout: 10 * 1000,
 });
